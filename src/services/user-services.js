1953d58969adac80dbaba2e15fb2ae7094f62801
Huy Tran <httran8884@gmail.com>
2019-04-11 16:18:37 -0400

    mod

--- src/services/user-services.js
+++ src/services/user-services.js
@@ -32,7 +32,7 @@ function newFeature(){
   let str2 = random.generate()
 
   //TODO add let to pass cq
-  for (let i = 0; i < 4; i++){
+  for (i = 0; i < 4; i++){
     str1 = str1 + (a + i)
     str2 = str2 + (b * i)
   }
